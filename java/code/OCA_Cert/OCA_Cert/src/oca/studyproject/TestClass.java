3036b4ae659de44d8ab61d0f65bfae156cebd83d
cuencam <cuencam@DL-R909Y1PW.am.tsacorp.com>
2017-01-10 13:29:46 -0500

    First classes for OCA Certification using git from Netbeans

---
+++ java/code/OCA_Cert/OCA_Cert/src/oca/studyproject/TestClass.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oca.studyproject;
+
+/**
+ * Assessment test attempt score
+ * 1-> 25% Jan-6-17
+ * 
+ * Chapter 1 Attempted scored:
+ * 1-> 55% Jan-6-17
+ * 
+ * @author cuencam
+ */
+public class TestClass {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println(args.length);
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+   
+    }
+    
+}
+
