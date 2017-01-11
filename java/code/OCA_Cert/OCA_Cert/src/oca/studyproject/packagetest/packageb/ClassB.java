3ed03f00a18929a658faadd8b64a19efa6ef85a2
mauroqenk <maurocuenca05@gmail.com>
2017-01-10 23:23:51 -0500

    packagetest review

---
+++ java/code/OCA_Cert/OCA_Cert/src/oca/studyproject/packagetest/packageb/ClassB.java
@@ -0,0 +1,11 @@
+package oca.studyproject.packagetest.packageb;
+
+import oca.studyproject.packagetest.packagea.ClassA;
+
+public class ClassB 
+{
+    public static void main(String... args){
+        ClassA a;
+        System.out.println("Got it");
+    }
+}
